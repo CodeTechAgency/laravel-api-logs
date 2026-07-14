00b9d30b9f6461e3c9b5a64610d572213d4961f2
jfrosorio <jfrosorio@gmail.com>
2026-07-14 10:55:48 +0100

    test(testcase): define sqlite in-memory testing connection explicitly

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,6 +26,11 @@ abstract class TestCase extends Orchestra
     protected function defineEnvironment($app): void
     {
         $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
     }
 
     private function runMigrations(): void
